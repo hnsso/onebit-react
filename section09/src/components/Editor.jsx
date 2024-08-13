cb6acfae8c9591e356bfc2742ae7acb11ead1bf6
김현수 <152463185+hnsso@users.noreply.github.com>
2024-08-14 02:24:46 +0900

    hello

---
+++ section09/src/components/Editor.jsx
@@ -0,0 +1,54 @@
+import { useState, useRef } from "react";
+import "./Editor.css";
+
+// 우리가 부모컴포넌트인 App 컴포넌트에서 Props로 이벤트핸들러함수를 받아오고
+const Editor = ({ onCreate }) => {
+  // 우리가 Props로 함수를 받아오긴 했지만 input태그에 담을 content state가 없기 때문에
+  // useState(); 하나 생성
+  const [content, setContent] = useState("");
+  // => 그래서 우리는 초기값은 "빈배열" 이고 변수가 content이고 상태변화함수가 setContent인 useState함수를 만든다
+
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  // 사용자가 키보드를 눌렸을때 저장
+  const onKeydown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  // onSubmit 함수에서 우리가 유의 해야할 것
+  // 1 . input태그안 content가 null이면 안된다
+  // 2 . 만약에 null일 경우 내용을 다시 기입하겠끔 해야한다
+  // 3 . 그리고 content를 제대로 작성하고 제출버튼을 누르면 기존에 content가 ""이어야한다
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    } else {
+      onCreate(content); // onCreate함수에 content 저장
+      setContent(""); // 초기화
+    }
+  };
+
+  return (
+    <div className="Editor">
+      {/* 별도로 input태그에 content의 내용을 담고있는 state가 없어서
+          useState(); 하나 만들기 */}
+      <input
+        onKeyDown={onKeydown}
+        ref={contentRef}
+        value={content}
+        onChange={onChangeContent}
+        placeholder=" 새로운 Todo...."
+      />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  );
+};
+
+export default Editor;
--- section10/src/App.jsx
+++ section10/src/App.jsx
@@ -1,8 +1,9 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useReducer } from "react";
 import "./App.css";
 import Editor from "./components/Editor";
 import Header from "./components/Header";
 import List from "./components/List";
+// import Exam from "./components/Exam";
 
 const mockData = [
   // 임시데이터 객체 3가지를 만들어 주고 시작한다
@@ -26,46 +27,61 @@ const mockData = [
   },
 ];
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        item.id === action.targetId ? { ...item, isDone: !item.isDone } : item
+      );
+    case "DELETE":
+      return state.filter((item) => item.id !== action.targetId);
+    default:
+      return state;
+  }
+}
+
 const App = () => {
   // state를 이용해야하는 조상인 부모컴포넌트에서 관리해야한다.
   // todos에 배열형태로 List를 보관하기 !!
-  const [todos, setTodos] = useState(mockData);
+  // const [todos, setTodos] = useState(mockData); -- (1) useState
+
+  // useReducer
+  const [todos, dispatch] = useReducer(reducer, mockData);
+
   const idRef = useRef(3);
 
   // onCreate 함수를 만듬
   const onCreate = (content) => {
-    const newTodo = {
-      // useRef 객체를 사용해서 id의 고유의 값들을 만들어내자
-      id: idRef.current++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime(),
-    };
-
-    // 상태변화함수로 값을 넣어야한다
-    setTodos([newTodo, ...todos]);
-    // 우리가 추가하고자하는 데이터 / 원래 ...(스프레드 연산자)todos 정보들 닮고
+    dispatch({
+      type: "CREATE",
+      date: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    });
   };
 
   const onUpdate = (targetId) => {
-    // todos State의 값들 중에
-    // targetId와 일치하는 id를 갖는 투두 아이템의 isDone 변경
-    // 인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소의
-    // 데이터만 딱 바꾼 새로운 배열
-    setTodos(
-      todos.map((todo) =>
-        todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo
-      )
-    );
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId,
+    });
   };
 
   const onDelete = (targetId) => {
-    // 인수 : todos 배열에서 targetId와 일치하는 id를 갖는 요소만 삭제한 새로운 배열
-    setTodos(todos.filter((todo) => todo.id !== targetId));
+    dispatch({
+      type: "DELETE",
+      targetId: targetId,
+    });
   };
 
   return (
     <div className="App">
+      {/* <Exam /> */}
       <Header />
       {/* Editor컴포넌트에 props로 전달 */}
       <Editor onCreate={onCreate} />
